046ca45d41a172d546b40949489f319f657f145c
samcompz <samwambua25@gmail.com>
2023-01-05 12:41:23 +0300

    User level check disabled

--- add_group.php
+++ add_group.php
@@ -2,7 +2,7 @@
   $page_title = 'Add Group';
   require_once('includes/load.php');
   // Checkin What level user has permission to view this page
-   page_require_level(1);
+  //  page_require_level(1);
 ?>
 <?php
   if(isset($_POST['add'])){
--- admin.php
+++ admin.php
@@ -71,8 +71,8 @@
       <div class="panel">
         <div class="jumbotron text-center">
            <h1>Thank You! for your support and love.</h1>
-           <p> <strong>OSWA-INV v2</strong> way more better then <strong> v1 </strong>.
-           </br>If you have a question regarding the usage of this applications, please ask on <a href="https://www.facebook.com/oswapp" title="Facebook" target="_blank">Facebook</a> OSWA Fan page.</p>
+           <!-- <p> <strong>Mars SYSTEM v2</strong> way more better then <strong> v1 </strong>. -->
+           <!-- </br>If you have a question regarding the usage of this applications, please ask on <a href="https://www.facebook.com/oswapp" title="Facebook" target="_blank">Facebook</a> OSWA Fan page.</p> -->
 
         </div>
       </div>
@@ -84,7 +84,7 @@
        <div class="panel-heading">
          <strong>
            <span class="glyphicon glyphicon-th"></span>
-           <span>Highest Saleing Products</span>
+           <span>Highest Selling Products</span>
          </strong>
        </div>
        <div class="panel-body">
